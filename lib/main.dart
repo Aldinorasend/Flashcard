b7107de9f421681b2e74367c291990bf8e6a9b43
Aldinorasend <aldinorasend@gmail.com>
2024-10-25 18:01:53 +0700

    initializing project

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'screen/kategori.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flashcard',
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Timer to navigate to the MyHomePage after 3 seconds
+    Timer(Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Kategori()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:
+          Colors.blueAccent, // Background color of the splash screen
+      body: Center(
+        child:
+            Image.asset('lib/asset/logo.png', width: 200), // Add your logo here
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Text('Welcome to the App!'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/kategori.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class Kategori extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.only(left: 35.0, top: 20, right: 35.0),
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Wrap(
+                spacing: 20,
+                runSpacing: 20,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/1.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  GestureDetector(
+                      child: Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/2.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  )),
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/3.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/4.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/5.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/6.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/7.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/asset/kategori/8.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
+      )),
+    );
+  }
+}
